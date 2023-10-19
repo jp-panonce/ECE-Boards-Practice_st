b225fe8e5a590fb5d23791d54b074626bbd23ac3
jpasensi <jpasensi13@gmail.com>
2023-10-20 07:01:42 +0800

    added: questions data import as string added: support for question image added: support for latex and image of explanation

--- Practice.py
+++ Practice.py
@@ -3,6 +3,8 @@ import pandas as pd
 from random import randrange
 from random import shuffle
 import unicodedata
+from PIL import Image
+
 
 st.set_page_config(page_title="Practice MCQ",page_icon="📃",layout="wide")
 
@@ -12,10 +14,34 @@ def remove_control_characters(s):
 def showanswerButtonClicked():
     st.session_state['highlight_answer'] = True
 
-#get questions data
 
+st.markdown('''
+<style>
+.katex-html {
+    text-align: left;
+}
+</style>''',
+unsafe_allow_html=True
+)
+
+#get questions data
 if 'questions_df' not in st.session_state:
-    questions_df = pd.read_excel('data/questions.xlsx', sheet_name='ECE')
+    questions_df = pd.read_excel('data/questions.xlsx', sheet_name='ECE',converters={
+        'question_id':str,
+        'Subject':str,
+        'question_txt':str,
+        'question_img':str,
+        'mul_choice':str,
+        'ANS':str,
+        'A':str,
+        'B':str,
+        'C':str,
+        'D':str,
+        'explanation_txt':str,
+        'explanation_img':str,
+        'explanation_latex':str
+
+    })
     st.session_state['questions_df'] = questions_df
 else:
     questions_df = st.session_state['questions_df']    
@@ -40,27 +66,44 @@ if 'question_loaded' not in st.session_state:
 
     st.session_state['question_loaded'] = True
 
+    question_id = applicable_questions.loc[applicable_questions.index[questionidx_to_display], 'question_id']
     question_to_display = applicable_questions.loc[applicable_questions.index[questionidx_to_display], 'question_txt']
     letter_correct_answer = applicable_questions.loc[applicable_questions.index[questionidx_to_display], 'ANS']
     mult_choice = applicable_questions.loc[applicable_questions.index[questionidx_to_display], 'mul_choice']
-    correct_answer = remove_control_characters(applicable_questions.loc[applicable_questions.index[questionidx_to_display], letter_correct_answer])
     explanation_txt = applicable_questions.loc[applicable_questions.index[questionidx_to_display], 'explanation_txt']
+    question_img = applicable_questions.loc[applicable_questions.index[questionidx_to_display], 'question_img']
+    explanation_ltx = applicable_questions.loc[applicable_questions.index[questionidx_to_display], 'explanation_latex']
+    explanation_img = applicable_questions.loc[applicable_questions.index[questionidx_to_display], 'explanation_img']
 
     
+    st.session_state['question_id'] = question_id
     st.session_state['question_to_display'] = question_to_display
     st.session_state['letter_correct_answer'] = letter_correct_answer
     st.session_state['mult_choice'] = mult_choice
-    st.session_state['correct_answer'] = correct_answer
     st.session_state['explanation_txt'] = explanation_txt
+    st.session_state['question_img'] = question_img
+    st.session_state['explanation_ltx'] = explanation_ltx
+    st.session_state['explanation_img'] = explanation_img
 
-    
+    if mult_choice == "y":
+        correct_answer = remove_control_characters(applicable_questions.loc[applicable_questions.index[questionidx_to_display], letter_correct_answer])
+        st.session_state['correct_answer'] = correct_answer
+
 
 else:
+
+    question_id = st.session_state['question_id']
     question_to_display = st.session_state['question_to_display']
     letter_correct_answer = st.session_state['letter_correct_answer']
     mult_choice = st.session_state['mult_choice']
-    correct_answer = st.session_state['correct_answer']
     explanation_txt = st.session_state['explanation_txt']
+    question_img = st.session_state['question_img']
+    explanation_ltx = st.session_state['explanation_ltx']
+    explanation_img = st.session_state['explanation_img']
+
+    if mult_choice == "y":
+        correct_answer = st.session_state['correct_answer']
+        
 
 
 correct_choice = "-"
@@ -88,41 +131,75 @@ if mult_choice == "y":
 
     correct_choice = (["A","B","C","D"])[shuffled_choices.index(correct_answer)]
 else:
+
     print("not a multiple choice question")
 
-
-
-
-# st.dataframe(questions_df)
 st.markdown("#### " + question_to_display)
+if str(question_img) != "nan":
+    try:
+        q_img = Image.open("images/" + str(question_img))
+        st.image(q_img)
+    except:
+        st.write("(image can't be loaded)")
+        pass
 
-# display shuffled choices
-choice_to_display = []
-for idx_choice, letter_choice in enumerate(["A","B","C","D"]):
 
-    if (('highlight_answer' in st.session_state) & (shuffled_choices[idx_choice] == correct_answer)):
-        st.markdown("#### :green[" + str(letter_choice) + ". " + str(shuffled_choices[idx_choice]).strip().replace("_x000D_","") + "]")
+if mult_choice == "y":
+    # display shuffled choices
+    choice_to_display = []
+    for idx_choice, letter_choice in enumerate(["A","B","C","D"]):
+
+        if (('highlight_answer' in st.session_state) & (shuffled_choices[idx_choice] == correct_answer)):
+            st.markdown("#### :green[" + str(letter_choice) + ". " + str(shuffled_choices[idx_choice]).strip().replace("_x000D_","") + "]")
+        else:
+            st.markdown("#### " + str(letter_choice) + ". " + str(shuffled_choices[idx_choice]).strip().replace("_x000D_","") + "")
+else:
+    if (('highlight_answer' in st.session_state)):
+        st.markdown("### ANS: :green[" + str(letter_correct_answer) + "]")
     else:
-        st.markdown("#### " + str(letter_choice) + ". " + str(shuffled_choices[idx_choice]).strip().replace("_x000D_","") + "")
-
+        st.markdown("### ANS: ?")
 
 butt_col1, butt_col2, sp = st.columns([0.1,0.1,1])
 
-with butt_col1:
-    anoth_question_button = st.button('Another Question')
+anoth_question_button = st.button('Another Question')
+checkAnswer_button = st.button('Check Answer',on_click = showanswerButtonClicked)
+st.divider()
 
-with butt_col2:
-    checkAnswer_button = st.button('Check Answer',on_click = showanswerButtonClicked)
-
+# SHOW EXPLANATION
 if 'highlight_answer' in st.session_state:
     # highlight answer if MCQ or show if not MCQ
-    if bool(explanation_txt):
+    if str(explanation_txt) != "nan":
+        
         st.markdown("## :blue[EXPLANATION]")
+        st.write(str(explanation_txt))
+                
+        if bool(explanation_img):
 
-        st.text(str(explanation_txt))
+            expl_col1, expl_col2 = st.columns(2)
+
+            with expl_col1:
+                if str(explanation_ltx) != "nan":
+                    st.latex(str(explanation_ltx))
 
 
-    
+            with expl_col2:
+
+                if str(explanation_img) != "nan":
+                    try:
+                        e_img = Image.open("images/" + explanation_img)
+                        st.image(e_img)
+                    except:
+                        st.write("(image can't be loaded)")
+                        pass
+            
+            
+
+        else:
+            st.markdown("## :blue[EXPLANATION]")
+            st.text(str(explanation_txt))
+            if(str(explanation_ltx) != 'nan'):
+                st.latex(str(explanation_ltx))
+
 
 
 if anoth_question_button:
